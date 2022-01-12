3226957e8a1e6069237c0abb926d1582ece86f52
Clover <39840298+SmollClover@users.noreply.github.com>
2022-01-12 21:33:13 +0100

    Refactored Test to be more reliable with test data

--- test/Matrix.test.ts
+++ test/Matrix.test.ts
@@ -3,47 +3,90 @@
 import { Matrix } from '../src/class/Matrix';
 
 describe('Matrix', () => {
-	let matrix: Matrix;
-
 	test('Create a Matrix', () => {
-		matrix = new Matrix(1, 2);
+		const matrix = new Matrix(2, 3);
 
 		expect(matrix.data[0][1]).toEqual(0);
 	});
 
 	test('Set Matrix [0][1] to 5', () => {
+		const matrix = new Matrix(2, 3);
 		matrix.set(0, 1, 5);
 
 		expect(matrix.data[0][1]).toEqual(5);
 	});
 
 	test('Fill Matrix with random values', () => {
+		const matrix = new Matrix(2, 3);
 		matrix.rand();
 
 		expect(matrix.data[0][0]).not.toEqual(0);
 	});
 
 	test('Add 100 to Matrix [0][1]', () => {
-		const tempValue = matrix.data[0][1];
+		const matrix = new Matrix(2, 3);
+		matrix.data = [
+			[0, 1, 7],
+			[6, 9, 5],
+		];
 		matrix.add(100);
 
-		expect(matrix.data[0][1]).toEqual(tempValue + 100);
+		expect(matrix.data).toEqual([
+			[100, 101, 107],
+			[106, 109, 105],
+		]);
 	});
 
 	test('Multiply Matrix [0][1] by 5', () => {
-		const tempValue = matrix.data[0][1];
+		const matrix = new Matrix(2, 3);
+		matrix.data = [
+			[7, 9, 9],
+			[5, 6, 5],
+		];
 		matrix.mult(5);
 
-		expect(matrix.data[0][1]).toEqual(tempValue * 5);
+		expect(matrix.data).toEqual([
+			[35, 45, 45],
+			[25, 30, 25],
+		]);
 	});
 
 	test('Add Matrix to Matrix', () => {
-		const tempMatrix = new Matrix(1, 2);
-		tempMatrix.rand();
+		const a = new Matrix(2, 3);
+		const b = new Matrix(2, 3);
+		a.data = [
+			[3, 1, 1],
+			[1, 1, 4],
+		];
+		b.data = [
+			[0, 3, 3],
+			[1, 2, 1],
+		];
+		a.add(b);
 
-		const tempValue = matrix.data[0][1];
-		matrix.add(tempMatrix);
+		expect(a.data).toEqual([
+			[3, 4, 4],
+			[2, 3, 5],
+		]);
+	});
 
-		expect(matrix.data[0][1]).toEqual(tempValue + tempMatrix.data[0][1]);
+	test('Multiply Matrix to Matrix', () => {
+		const a = new Matrix(2, 3);
+		const b = new Matrix(3, 2);
+		a.data = [
+			[9, 8, 3],
+			[8, 6, 9],
+		];
+		b.data = [
+			[4, 4],
+			[3, 4],
+			[3, 1],
+		];
+
+		const c = a.mult(b);
+		expect(c?.data).toEqual([
+			[69, 71],
+			[77, 65],
+		]);
 	});
 });
