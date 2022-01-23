c9657601e6e5e20d494821b3ec99f1254d4d7445
Clover <39840298+SmollClover@users.noreply.github.com>
2022-01-23 18:04:08 +0100

    Implemented Matrix Math Functions in seperate Class and added Transpose

--- src/class/Matrix.ts
+++ src/class/Matrix.ts
@@ -2,9 +2,57 @@ import consola from 'consola';
 
 export type MatrixData = Array<Array<number>>;
 
+class MatrixMath {
+	public constructor() {}
+
+	public async add(a: Matrix, b: Matrix): Promise<Matrix> {
+		if (a.rows !== b.rows || a.columns !== b.columns) throw new Error('Matrix must have same row and column length');
+		const result = new Matrix(a.rows, a.columns);
+
+		for (let i = 0; i < a.rows; i++) {
+			for (let j = 0; j < a.columns; j++) {
+				result.data[i][j] = a.data[i][j] + b.data[i][j];
+			}
+		}
+
+		return result;
+	}
+
+	public async multiply(a: Matrix, b: Matrix): Promise<Matrix> {
+		if (a.columns !== b.rows) throw new Error('Columns of Matrix A must match Rows of Matrix B');
+		const result = new Matrix(a.rows, b.columns);
+
+		for (let i = 0; i < result.rows; i++) {
+			for (let j = 0; j < result.columns; j++) {
+				let sum = 0;
+
+				for (let k = 0; k < a.columns; k++) {
+					sum += a.data[i][k] * b.data[k][j];
+				}
+
+				result.data[i][j] = sum;
+			}
+		}
+
+		return result;
+	}
+
+	public async transpose(a: Matrix): Promise<Matrix> {
+		const result = new Matrix(a.columns, a.rows);
+
+		for (let i = 0; i < a.rows; i++) {
+			for (let j = 0; j < a.columns; j++) {
+				result.data[j][i] = a.data[i][j];
+			}
+		}
+
+		return result;
+	}
+}
+
 class Matrix {
-	private rows: number;
-	private columns: number;
+	public rows: number;
+	public columns: number;
 	public data: MatrixData;
 
 	public constructor(rows: number, columns: number) {
@@ -20,13 +68,13 @@ class Matrix {
 		}
 	}
 
-	public set(row: number, column: number, n: number): void {
+	public async set(row: number, column: number, n: number): Promise<void> {
 		if (this.rows < row || this.columns < column) return;
 
 		this.data[row][column] = n;
 	}
 
-	public rand(): void {
+	public async rand(): Promise<void> {
 		for (let i = 0; i < this.rows; i++) {
 			for (let j = 0; j < this.columns; j++) {
 				this.data[i][j] = Math.floor(Math.random() * 9) + 1;
@@ -34,50 +82,21 @@ class Matrix {
 		}
 	}
 
-	public add(n: number | Matrix): void {
-		if (n instanceof Matrix) {
-			for (let i = 0; i < this.rows; i++) {
-				for (let j = 0; j < this.columns; j++) {
-					this.data[i][j] += n.data[i][j];
-				}
-			}
-		} else {
-			for (let i = 0; i < this.rows; i++) {
-				for (let j = 0; j < this.columns; j++) {
-					this.data[i][j] += n;
-				}
+	public async add(n: number): Promise<void> {
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.columns; j++) {
+				this.data[i][j] += n;
 			}
 		}
 	}
 
-	public mult(n: number | Matrix): undefined | Matrix {
-		if (n instanceof Matrix) {
-			if (this.columns !== n.rows) {
-				consola.error('Columns of Matrix A must match Rows of Matrix B!');
-				throw new Error('Matrix not valid for multiplication');
-			}
-
-			const result: Matrix = new Matrix(this.rows, n.columns);
-			for (let i = 0; i < result.rows; i++) {
-				for (let j = 0; j < result.columns; j++) {
-					let sum = 0;
-					for (let k = 0; k < this.columns; k++) {
-						sum += this.data[i][k] * n.data[k][j];
-					}
-
-					result.data[i][j] = sum;
-				}
-			}
-
-			return result;
-		} else {
-			for (let i = 0; i < this.rows; i++) {
-				for (let j = 0; j < this.columns; j++) {
-					this.data[i][j] *= n;
-				}
+	public async multiply(n: number): Promise<void> {
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.columns; j++) {
+				this.data[i][j] *= n;
 			}
 		}
 	}
 }
 
-export { Matrix };
+export { Matrix, MatrixMath };
--- test/Matrix.test.ts
+++ test/Matrix.test.ts
@@ -1,29 +1,30 @@
 /// <reference types="jest" />
 
-import { Matrix } from '../src/class/Matrix';
+import { Matrix, MatrixMath } from '../src/class/Matrix';
+const MMath = new MatrixMath();
 
 describe('Matrix', () => {
-	test('Create a Matrix', () => {
+	test('Create a Matrix', async () => {
 		const matrix = new Matrix(2, 3);
 
 		expect(matrix.data[0][1]).toEqual(0);
 	});
 
-	test('Set Matrix [0][1] to 5', () => {
+	test('Set Matrix [0][1] to 5', async () => {
 		const matrix = new Matrix(2, 3);
 		matrix.set(0, 1, 5);
 
 		expect(matrix.data[0][1]).toEqual(5);
 	});
 
-	test('Fill Matrix with random values', () => {
+	test('Fill Matrix with random values', async () => {
 		const matrix = new Matrix(2, 3);
 		matrix.rand();
 
 		expect(matrix.data[0][0]).not.toEqual(0);
 	});
 
-	test('Add 100 to Matrix [0][1]', () => {
+	test('Add 100 to Matrix [0][1]', async () => {
 		const matrix = new Matrix(2, 3);
 		matrix.data = [
 			[0, 1, 7],
@@ -37,13 +38,13 @@ describe('Matrix', () => {
 		]);
 	});
 
-	test('Multiply Matrix [0][1] by 5', () => {
+	test('multiplyiply Matrix [0][1] by 5', async () => {
 		const matrix = new Matrix(2, 3);
 		matrix.data = [
 			[7, 9, 9],
 			[5, 6, 5],
 		];
-		matrix.mult(5);
+		matrix.multiply(5);
 
 		expect(matrix.data).toEqual([
 			[35, 45, 45],
@@ -51,7 +52,7 @@ describe('Matrix', () => {
 		]);
 	});
 
-	test('Add Matrix to Matrix', () => {
+	test('Add Matrix to Matrix', async () => {
 		const a = new Matrix(2, 3);
 		const b = new Matrix(2, 3);
 		a.data = [
@@ -62,15 +63,15 @@ describe('Matrix', () => {
 			[0, 3, 3],
 			[1, 2, 1],
 		];
-		a.add(b);
+		const c = await MMath.add(a, b);
 
-		expect(a.data).toEqual([
+		expect(c.data).toEqual([
 			[3, 4, 4],
 			[2, 3, 5],
 		]);
 	});
 
-	test('Multiply Matrix to Matrix', () => {
+	test('Multiply Matrix to Matrix', async () => {
 		const a = new Matrix(2, 3);
 		const b = new Matrix(3, 2);
 		a.data = [
@@ -82,11 +83,26 @@ describe('Matrix', () => {
 			[3, 4],
 			[3, 1],
 		];
+		const c = await MMath.multiply(a, b);
 
-		const c = a.mult(b);
-		expect(c?.data).toEqual([
+		expect(c.data).toEqual([
 			[69, 71],
 			[77, 65],
+		]);
+	});
+
+	test('Transpose Matrix', async () => {
+		const a = new Matrix(2, 3);
+		a.data = [
+			[5, 6, 9],
+			[1, 3, 4],
+		];
+		const b = await MMath.transpose(a);
+
+		expect(b.data).toEqual([
+			[5, 1],
+			[6, 3],
+			[9, 4],
 		]);
 	});
 });
